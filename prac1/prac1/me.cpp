9a2fc0db81e8b123b7c2790ced3e8dbde1d050b6
ProJames <james__eun@naver.com>
2020-04-20 16:52:30 +0900

    1-1CPP
    실습문제1

---
+++ prac1/prac1/me.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	cout << "자신의학과:" << "컴퓨터공학과" << endl;
+	cout << "나이:" << 30 << endl;
+	cout << "직업:" << "대통령" << endl;
+
+	cout << 1 << "에서" << 10 << "까지 더한 결과는 " << 55 << "입니다" << endl;
+
+	cout << "*" << endl;
+	cout << "**" << endl;
+	cout << "***" << endl;
+	cout << "****" << endl;
+
+	getchar();
+
+
+}
